c14c3ebc3415192432e4b6150818831c28d3cab3
Vittorio Meloni <vittorio.meloni@crs4.it>
2018-06-13 17:31:56 +0200

    Change to generate development certs with historical characters

--- certs/generate_ts_cns_client_dev_certificate.sh
+++ certs/generate_ts_cns_client_dev_certificate.sh
@@ -18,31 +18,41 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-CLIENT_NAME=$1
 PASSKEY=hgwpwd
 WEB_CA_CERT=ca/web/certs/ca/web.cert.pem
 WEB_CA_KEY=ca/web/certs/ca/web.key.pem
 ROOT_CA_CERT=ca/root/ca.cert.pem
 
-CLIENT_BASE_DIR=ca/web/certs/${CLIENT_NAME}
-mkdir -p ${CLIENT_BASE_DIR}
-CLIENT_KEYSTORE=${CLIENT_BASE_DIR}/keystore.jks
-CLIENT_TRUSTSTORE=${CLIENT_BASE_DIR}/truststore.jks
-CLIENT_CERT=${CLIENT_BASE_DIR}/cert.pem
-CLIENT_KEY=${CLIENT_BASE_DIR}/key.pem
-CLIENT_CSR=${CLIENT_BASE_DIR}/csr.pem
-CLIENT_P12=${CLIENT_BASE_DIR}/cert.p12
+declare -A PERSONS
 
-if [ -z "${CLIENT_NAME}" ]; then
-	echo "You should specify the client name"
-	exit 1
-fi
+function create_cert() {
+    CLIENT_NAME=$1
+    NAME=$2
+    SURNAME=$3
+    ID=$4
 
-openssl genrsa -des3 -out ${CLIENT_KEY} 4096
-openssl req -new -key ${CLIENT_KEY} -out ${CLIENT_CSR} -subj /C=IT/CN=AAABBB12C34D567E\/0000000000000000.G0tzapq6WhWHR5qmrzpDlJxARELM=/GN=PAOLINO/SN=PAPERINO
+    CLIENT_BASE_DIR=ca/web/certs/${CLIENT_NAME}
+    mkdir -p ${CLIENT_BASE_DIR}
+    CLIENT_KEYSTORE=${CLIENT_BASE_DIR}/keystore.jks
+    CLIENT_TRUSTSTORE=${CLIENT_BASE_DIR}/truststore.jks
+    CLIENT_CERT=${CLIENT_BASE_DIR}/cert.pem
+    CLIENT_KEY=${CLIENT_BASE_DIR}/key.pem
+    CLIENT_CSR=${CLIENT_BASE_DIR}/csr.pem
+    CLIENT_P12=${CLIENT_BASE_DIR}/cert.p12
 
-echo "Signing the client certificate with the CA key"
+    openssl genrsa -des3 -out ${CLIENT_KEY} 4096
+    openssl req -new -key ${CLIENT_KEY} -out ${CLIENT_CSR} \
+        -subj /C=IT/CN=${ID}\/0000000000000000.oXPnbQvnvQANlkxAg/GN=${NAME}/SN=${SURNAME}
 
-openssl x509 -req -CA ${WEB_CA_CERT} -CAkey ${WEB_CA_KEY} -in ${CLIENT_CSR} -out ${CLIENT_CERT} -days 365 \
-    -CAcreateserial -passin pass:${PASSKEY}
-openssl pkcs12 -export -out ${CLIENT_P12} -inkey ${CLIENT_KEY} -in ${CLIENT_CERT} -CAfile ${WEB_CA_KEY}
+    echo "Signing the client certificate with the CA key"
+
+    openssl x509 -req -CA ${WEB_CA_CERT} -CAkey ${WEB_CA_KEY} -in ${CLIENT_CSR} -out ${CLIENT_CERT} -days 365 \
+        -CAcreateserial -passin pass:${PASSKEY}
+    openssl pkcs12 -export -out ${CLIENT_P12} -inkey ${CLIENT_KEY} -in ${CLIENT_CERT} -CAfile ${WEB_CA_KEY}
+
+}
+create_cert "garibaldi" "Giuseppe" "Garibaldi" "GRBGPP87L04L741X"
+create_cert "cesare" "Giulio" "Cesare" "CSRGGL44L13H501E"
+create_cert "lovelace" "Ada" "Lovelace" "LVLDAA85T50G702B"
+create_cert "darco" "Giovanna" "D'Arco" "DRCGNN12A46A326K"
+
