5773f155bbca5f6adaf311b6264193b3f0dc4744
Vittorio Meloni <vittorio.meloni@crs4.it>
2018-07-30 12:20:19 +0200

    Fixes docker-entrypoint for dispatccher

--- docker/images/hgw_dispatcher/resources/docker-entrypoint.sh
+++ docker/images/hgw_dispatcher/resources/docker-entrypoint.sh
@@ -3,10 +3,10 @@
 # It checks if there are files in the development directory (1 because ls -l print at least the number of files)
 if [ `ls -l /container/devservice | wc -l` == 1 ]; then
     echo "USING PROD DIR"
-    export BASE_SERVICE_DIR=/container/devservice
+    export BASE_SERVICE_DIR=/container/service
 else
     echo "USING DEV DIR"
-    export BASE_SERVICE_DIR=/container/service
+    export BASE_SERVICE_DIR=/container/devservice
 fi
 cd ${BASE_SERVICE_DIR}
 
