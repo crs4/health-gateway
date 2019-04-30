132637d0b104e9452e9a5b38caee742c685e247c
Vittorio Meloni <vittorio.meloni@crs4.it>
2019-04-30 14:15:41 +0200

    Changes entrypoint to load data from fixtures also in development

--- docker/images/web_base/resources/custom-docker-entrypoint.sh
+++ docker/images/web_base/resources/custom-docker-entrypoint.sh
@@ -22,40 +22,40 @@ if [ `ls -l ${DEV_DJANGO_DIR} | wc -l` == 1 ]; then
     echo "USING PROD DIR"
     export BASE_SERVICE_DIR=${DJANGO_DIR}
     USER=gunicorn
-    cd ${BASE_SERVICE_DIR}
-
-    INITIALIZED="/container/initialized"
-
-    if [ ! -e "$INITIALIZED" ]; then
-        python3 manage.py migrate
-        FIXTURES_DIR=/container/fixtures
-
-        if [ -d ${FIXTURES_DIR} ]; then
-            if [ -z ${ENVIRONMENT} ] || [ "${ENVIRONMENT}" == "DEVELOPMENT"  ]; then
-                python3 manage.py loaddata ${FIXTURES_DIR}/development_data.json
-            elif [[ "${ENVIRONMENT}" == "STAGE" ]]; then
-                python3 manage.py loaddata ${FIXTURES_DIR}/stage_data.json
-            elif [[ "${ENVIRONMENT}" == "PRODUCTION" ]]; then
-                python3 manage.py loaddata ${FIXTURES_DIR}/production_data.json
-            fi
-        fi
-
-        if [[ ! -z ${TEST} ]]; then
-            TEST_FIXTURES_DIR=/container/test_fixtures
-            if [ -d ${TEST_FIXTURES_DIR} ]; then
-                for fixture in `ls ${TEST_FIXTURES_DIR}/*.json`; do
-                    python3 manage.py loaddata ${fixture}
-                done
-            fi
-        fi
-
-        touch ${INITIALIZED}
-    fi
 else
     echo "USING DEV DIR"
-    USER=root
     export BASE_SERVICE_DIR=${DEV_DJANGO_DIR}
-    cd ${BASE_SERVICE_DIR}
+    USER=root
+fi
+
+cd ${BASE_SERVICE_DIR}
+
+INITIALIZED="/container/initialized"
+
+if [ ! -e "$INITIALIZED" ]; then
+    python3 manage.py migrate
+    FIXTURES_DIR=/container/fixtures
+
+    if [ -d ${FIXTURES_DIR} ]; then
+        if [ -z ${ENVIRONMENT} ] || [ "${ENVIRONMENT}" == "DEVELOPMENT"  ]; then
+            python3 manage.py loaddata ${FIXTURES_DIR}/development_data.json
+        elif [[ "${ENVIRONMENT}" == "STAGE" ]]; then
+            python3 manage.py loaddata ${FIXTURES_DIR}/stage_data.json
+        elif [[ "${ENVIRONMENT}" == "PRODUCTION" ]]; then
+            python3 manage.py loaddata ${FIXTURES_DIR}/production_data.json
+        fi
+    fi
+
+    if [[ ! -z ${TEST} ]]; then
+        TEST_FIXTURES_DIR=/container/test_fixtures
+        if [ -d ${TEST_FIXTURES_DIR} ]; then
+            for fixture in `ls ${TEST_FIXTURES_DIR}/*.json`; do
+                python3 manage.py loaddata ${fixture}
+            done
+        fi
+    fi
+
+    touch ${INITIALIZED}
 fi
 
 if [ "$1" == "test" ]; then
