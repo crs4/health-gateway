36bd3015750da1fa5b0522c0604689b6000b358e
fabio <fabio.veronese@inpeco.com>
2019-11-18 16:37:30 +0100

    improved setup script and added sudo permissions for deletion

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,4 +1,39 @@
 #!/usr/bin/env bash
+
+function usage {
+    echo "Creates certificates for CAs and for services. By default it creates web and kafka certs for consentmanager hgwfrontend hgwbackend. If you need other services specify them"
+    echo "usage: ${0} [-s] [-h] [list of additional services]"
+    echo " -o       specify output dir. If not present it will use the current dir"
+    echo " -h       print this message"
+}
+
+if [ $# -ge 1 ]; then
+    case "$1" in
+        -h)
+            usage
+            exit 1
+            ;;
+        -o)
+            if [ "$#" = 1 ]; then
+                echo ERROR: Missing param for -o option
+                exit 1
+            fi
+            OUTPUT_DIR=$2
+            ;;
+        -c|--clean)
+            echo "Removing certs/ca"
+            sudo rm -rf certs/ca
+            echo "certs/kafka"
+            sudo rm -rf certs/kafka
+            echo "certs/root"
+            sudo rm -rf certs/root
+            echo "certs/web"
+            sudo rm -rf certs/web
+            echo "...done!"
+            sleep 2
+    esac
+fi
+
 if [ ! -d "djangosaml2" ]; then
   git clone -b develop https://github.com/crs4/djangosaml2.git
 fi
@@ -6,4 +41,9 @@ fi
 cd djangosaml2 && python3 setup.py install
 cd ../certs
 
+keytool 2>/dev/null || sudo apt install openjdk-13-jre-headless -y
+
 ./generate_development.sh
+
+mkdir -p ca/kafka/certs
+sudo cp -r kafka/certs/* ca/kafka/certs
